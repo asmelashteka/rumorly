83f56115e74687a5cdf1c6032edd52926eb43e68
jayanthgundam <jayanthgundam@users.noreply.github.com>
2017-01-31 09:22:51 +0100

    Update test_stat_features.py

--- tests/test_stat_features.py
+++ tests/test_stat_features.py
@@ -6,6 +6,7 @@ from datasketch import MinHash, MinHashLSH
 from collections import Counter
 from functools import reduce
 from scipy import stats as scistat
+from sklearn import svm
 import networkx as nx
 import matplotlib.pyplot as plt
 import sys
@@ -24,6 +25,7 @@ for line in open('abc.txt'):
             except:
                  pass
 signal_tweets=[]
+non_signal_tweets=[]
 
 for each in tweets:
     texts=each['text']
@@ -33,6 +35,14 @@ for each in tweets:
         signal_tweets.append(each)
     else:
         pass
+for each in tweets:
+    texts=each['text']
+    sentence=(texts.translate(non_bmp_map))
+    mat=re.match('(is(that|this|it)true?)|(real|really?|unconfirmed)|(rumor|debunk)|((this|that|it)is not true)|wh[a]*t[?!][?]*',sentence)
+    if mat:
+        pass
+    else:
+        non_signal_tweets.append(each)
 
 
 
