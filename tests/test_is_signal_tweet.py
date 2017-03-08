c655f69f6d065ae02bf9669f61abf8fa53317fea
jayanthgundam <jayanthgundam@users.noreply.github.com>
2017-03-08 21:25:17 +0100

    Update test_is_signal_tweet.py

--- tests/test_is_signal_tweet.py
+++ tests/test_is_signal_tweet.py
@@ -12,8 +12,6 @@ pytest and see the test results.
 Remove all output and report etc.
 
 Below is an example for Test-case1
-"""
-
 
 def test_name_exists():
     """one line description of what this test case is checking"""
@@ -21,99 +19,37 @@ def test_name_exists():
     assert is_signal_tweet(t1) == True
 
 """
-"Test-case1"
+#Test-Case1
 def test_is_signal_tweet():
+    """Check whether the statement is a signal_tweet"""
     t1 = "Is it true that Obama found  a bomb?"
     assert is_signal_tweet(t1) == True
-"output:   
-C:\Users\JAYANTH\Desktop\proj>py.test test_is_signal_tweet.py
-============================= test session starts =============================
-platform win32 -- Python 3.5.2, pytest-3.0.6, py-1.4.32, pluggy-0.4.0
-rootdir: C:\Users\JAYANTH\Desktop\proj, inifile:
-collected 1 items
-
-test_is_signal_tweet.py .
-
-========================== 1 passed in 1.15 seconds ===========================
-"
 
 
 
-"Test-Case2"
+#Test-Case2
+
 def test_is_signal_tweet():
+    """Check whether the statement is a signal_tweet"""
     t1="isis is a rumor"
     assert is_signal_tweet(t1)==True
-"output:
-C:\Users\JAYANTH\Desktop\proj>py.test test_is_signal_tweet.py
-============================= test session starts =============================
-platform win32 -- Python 3.5.2, pytest-3.0.6, py-1.4.32, pluggy-0.4.0
-rootdir: C:\Users\JAYANTH\Desktop\proj, inifile:
-collected 1 items
-test_is_signal_tweet.py .
-
-========================== 1 passed in 1.17 seconds ===========================
-"
 
 
-"Test-Case3"
+#Test-Case3
 def test_is_signal_tweet():
+    """Check whether the statement is a signal_tweet"""
     t1="isis"
     assert is_signal_tweet(t1)==True
-"output:
-================================== FAILURES ===================================
-____________________________ test_is_signal_tweet _____________________________
 
-    def test_is_signal_tweet():
-        t1="isis"
->       assert is_signal_tweet(t1)==True
-E       assert False == True
-E        +  where False = is_signal_tweet('isis')
-
-test_is_signal_tweet.py:7: AssertionError
-========================== 1 failed in 1.30 seconds ===========================
-"
-
-
-"Test-Case4"
+#Test-Case4
 def test_is_signal_tweet():
+    """Check whether the statement is a signal_tweet"""
     t1="whattttttt? Is that true?"
     assert is_signal_tweet(t1)==True
-    
-"output:
-C:\Users\JAYANTH\Desktop\proj>py.test test_is_signal_tweet.py
-============================= test session starts =============================
-platform win32 -- Python 3.5.2, pytest-3.0.6, py-1.4.32, pluggy-0.4.0
-rootdir: C:\Users\JAYANTH\Desktop\proj, inifile:
-collected 1 items
 
-test_is_signal_tweet.py .
-
-========================== 1 passed in 1.15 seconds ===========================
-"
-
-
-
-
-
-"Test-case5"
+#Test-case5
 def test_is_signal_tweet():
+    """Check whether the statement is a signal_tweet"""
     t1="it is not true"
     assert is_signal_tweet(t1)==True
 
-"output:
-C:\Users\JAYANTH\Desktop\proj>py.test test_is_signal_tweet.py
-============================= test session starts =============================
-platform win32 -- Python 3.5.2, pytest-3.0.6, py-1.4.32, pluggy-0.4.0
-rootdir: C:\Users\JAYANTH\Desktop\proj, inifile:
-collected 1 items
-
-test_is_signal_tweet.py .
-
-========================== 1 passed in 1.12 seconds ===========================
-"
-
-
-
-
-
-"""
