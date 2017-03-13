9e5e0b927714d945ff7f06cf4df2c845240ea5d8
jayanthgundam <jayanthgundam@users.noreply.github.com>
2017-03-13 12:13:36 +0100

    Update test_minhash_tweet.py

--- tests/test_minhash_tweet.py
+++ tests/test_minhash_tweet.py
@@ -3,14 +3,17 @@ import pytest
 from minhash2 import *
 #test-case1
 def test_gen_minhash_tweet1():
+    """Check whether the function is generating a minhash"""
     sent="a"
     assert minhash(sent)==81314
     
 def test_gen_minhash_tweet2():
+    """Check whether the function is generating a minhash"""
     sent="generate a sentence"
     assert minhash(sent)==45364
 
 def test_gen_minhash_tweet3():
+    """Check whether the function is generating a minhash"""
     sent="i"
     assert minhash(sent)==73763
     
