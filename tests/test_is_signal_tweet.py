7e4f86d00b3fdea971f256ad44c8e7faafefd34d
jayanthgundam <jayanthgundam@users.noreply.github.com>
2017-03-13 12:09:18 +0100

    Update test_is_signal_tweet.py

--- tests/test_is_signal_tweet.py
+++ tests/test_is_signal_tweet.py
@@ -21,7 +21,7 @@ def test_name_exists():
 """
 #Test-Case1
 def test_is_signal_tweet():
-    """Check whether the statement is a signal_tweet"""
+    """Check whether the statement contains signal pattern-is it true"""
     t1 = "Is it true that Obama found  a bomb?"
     assert is_signal_tweet(t1) == True
 
@@ -30,26 +30,26 @@ def test_is_signal_tweet():
 #Test-Case2
 
 def test_is_signal_tweet():
-    """Check whether the statement is a signal_tweet"""
+    """Check whether the statement contains signal pattern-rumor"""
     t1="isis is a rumor"
     assert is_signal_tweet(t1)==True
 
 
 #Test-Case3
 def test_is_signal_tweet():
-    """Check whether the statement is a signal_tweet"""
+    """Check whether the statement contains signal pattern-is"""
     t1="isis"
     assert is_signal_tweet(t1)==True
 
 #Test-Case4
 def test_is_signal_tweet():
-    """Check whether the statement is a signal_tweet"""
+    """Check whether the statement contains signal pattern-what?"""
     t1="whattttttt? Is that true?"
     assert is_signal_tweet(t1)==True
 
 #Test-case5
 def test_is_signal_tweet():
-    """Check whether the statement is a signal_tweet"""
+    """Check whether the statement contains signal pattern-it is not true"""
     t1="it is not true"
     assert is_signal_tweet(t1)==True
 
