3b0956914299ce736dc0be3d4e20cbbad1fd6100
jayanthgundam <jayanthgundam@users.noreply.github.com>
2017-03-21 11:45:51 +0100

    Update test_is_signal_tweet.py

--- tests/test_is_signal_tweet.py
+++ tests/test_is_signal_tweet.py
@@ -3,25 +3,9 @@ import pytest
 from rumorly import is_signal_tweet
 
 
-#TODO
-"""
-Change all your test suit such that
-(i) there's a description of what's being tested
-(ii) they are proper python scripts. i.e., one can type
-pytest and see the test results.
-Remove all output and report etc.
-
-Below is an example for Test-case1
-
-def test_name_exists():
-    """one line description of what this test case is checking"""
-    t1 = "Is it true that Obama found  a bomb?"
-    assert is_signal_tweet(t1) == True
-
-"""
 #Test-Case1
-def test_is_signal_tweet():
-    """Check whether the statement contains signal pattern-is it true"""
+def test_is_signal_tweet_isittrue():
+    """Check whether the statement is a signal_tweet"""
     t1 = "Is it true that Obama found  a bomb?"
     assert is_signal_tweet(t1) == True
 
@@ -29,27 +13,31 @@ def test_is_signal_tweet():
 
 #Test-Case2
 
-def test_is_signal_tweet():
-    """Check whether the statement contains signal pattern-rumor"""
-    t1="isis is a rumor"
+def test_is_signal_tweet_isis_itisrumor():
+    """Check whether the statement is a signal_tweet"""
+    t1="is it is a rumor"
     assert is_signal_tweet(t1)==True
 
 
 #Test-Case3
-def test_is_signal_tweet():
-    """Check whether the statement contains signal pattern-is"""
+def test_is_signal_tweet_not_isis():
+    """Check whether the statement is a signal_tweet"""
     t1="isis"
-    assert is_signal_tweet(t1)==True
+    assert is_signal_tweet(t1)==False
 
 #Test-Case4
-def test_is_signal_tweet():
-    """Check whether the statement contains signal pattern-what?"""
+def test_is_signal_tweet_whattt():
+    """Check whether the statement is a signal_tweet"""
     t1="whattttttt? Is that true?"
     assert is_signal_tweet(t1)==True
 
 #Test-case5
-def test_is_signal_tweet():
-    """Check whether the statement contains signal pattern-it is not true"""
+def test_is_signal_tweet_itisnottrue():
+    """Check whether the statement is a signal_tweet"""
     t1="it is not true"
     assert is_signal_tweet(t1)==True
-
+#test-case6
+def test_is_signal_tweet_not_realize():
+    """check whether statement is a signal_tweet"""
+    t1="realize"
+    assert is_signal_tweet(t1)==False
