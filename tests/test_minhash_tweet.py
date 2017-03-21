a74cf5b9debf442936a391135e99c08fca1158fc
jayanthgundam <jayanthgundam@users.noreply.github.com>
2017-03-21 11:47:17 +0100

    Update test_minhash_tweet.py

--- tests/test_minhash_tweet.py
+++ tests/test_minhash_tweet.py
@@ -1,19 +1,37 @@
 import pytest
 
-from minhash2 import *
+from rumorly import *
 #test-case1
 def test_gen_minhash_tweet1():
-    """Check whether the function is generating a minhash"""
+    #check whether the function generates a minhash
     sent="a"
-    assert minhash(sent)==81314
+    tweet_id=232444
+    assert minhash(sent,tweet_id,signal_minhashes,lsh_signal) in signal_minhashes.values()
+    
+
     
 def test_gen_minhash_tweet2():
-    """Check whether the function is generating a minhash"""
-    sent="generate a sentence"
-    assert minhash(sent)==45364
+    #check whether the function generates a minhash
+    sent="there is a accident in washington"
+    tweet_id=83746464
+    assert minhash(sent,tweet_id,signal_minhashes,lsh_signal) in signal_minhashes.values()
 
 def test_gen_minhash_tweet3():
-    """Check whether the function is generating a minhash"""
-    sent="i"
-    assert minhash(sent)==73763
-    
+    #check whether the function generates a minhash
+    sent="876577655446788"
+    tweet_id=6557767
+    assert minhash(sent,tweet_id,signal_minhashes,lsh_signal) in signal_minhashes.values()
+
+def test_gen_minhash_tweet4():
+    #check whether the function generates a minhash and index the minhash in lsh
+    sent="hello braunschweig how are you"
+    tweet_id=9849385359
+    minhash(sent,tweet_id,signal_minhashes,lsh_signal)
+    assert "9849385359" in lsh_signal
+
+def test_gen_minhash_tweet5():
+    #check whether the function generates a minhash and index the minhash in lsh
+    sent="berlin is a vibrant city"
+    tweet_id=9666050667
+    minhash(sent,tweet_id,signal_minhashes,lsh_signal)
+    assert "9666050667" in lsh_signal
