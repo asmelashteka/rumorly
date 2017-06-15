21499f68141f11a96a068c1c5bcb6f32b679bd68
jayanthgundam <jayanthgundam@users.noreply.github.com>
2017-06-15 09:41:57 +0200

    Create test_is_signal_tweet.py

--- tests/test_is_signal_tweet.py
+++ tests/test_is_signal_tweet.py
@@ -2,42 +2,60 @@ import pytest
 
 from rumorly import is_signal_tweet
 
-
-#Test-Case1
 def test_is_signal_tweet_isittrue():
     """Check whether the statement is a signal_tweet"""
     t1 = "Is it true that Obama found  a bomb?"
     assert is_signal_tweet(t1) == True
 
 
-
-#Test-Case2
-
 def test_is_signal_tweet_isis_itisrumor():
     """Check whether the statement is a signal_tweet"""
     t1="is it is a rumor"
     assert is_signal_tweet(t1)==True
+    
 
+def test_is_signal_tweet_isittrue_braces():
+    t1="(is it true?)"
+    assert is_signal_tweet(t1)==True
 
-#Test-Case3
-def test_is_signal_tweet_not_isis():
-    """Check whether the statement is a signal_tweet"""
-    t1="isis"
+def test_is_signal_tweet_realize():
+    t1="i realize it at the end"
     assert is_signal_tweet(t1)==False
 
-#Test-Case4
-def test_is_signal_tweet_whattt():
-    """Check whether the statement is a signal_tweet"""
-    t1="whattttttt? Is that true?"
+
+def test_is_signal_tweet_brumor():
+    t1="brumor"
+    assert is_signal_tweet(t1)==False
+    
+def test_is_signal_tweet_disconfirmed():
+    t1="disconfirmed"
+    assert is_signal_tweet(t1)==False
+
+def test_is_signal_tweet_really():
+    t1="really"
+    assert is_signal_tweet(t1)==False
+
+def test_is_signal_tweet_debunked():
+    t1="debunked"
     assert is_signal_tweet(t1)==True
 
-#Test-case5
-def test_is_signal_tweet_itisnottrue():
-    """Check whether the statement is a signal_tweet"""
-    t1="it is not true"
+def test_is_signal_tweet_rumors():
+    t1="rumors"
     assert is_signal_tweet(t1)==True
-#test-case6
-def test_is_signal_tweet_not_realize():
-    """check whether statement is a signal_tweet"""
-    t1="realize"
+    
+def test_is_signal_tweet_what():
+    t1="what"
+    assert is_signal_tweet(t1)==False
+
+
+def test_is_signal_tweet_what_quest():
+    t1="what?"
+    assert is_signal_tweet(t1)==True
+
+def test_is_signal_tweet_what_exclam():
+    t1="what!"
+    assert is_signal_tweet(t1)==True
+
+def test_is_signal_tweet_isis():
+    t1="isis"
     assert is_signal_tweet(t1)==False
