dd64111544339e7976872a832bedced13a56ed4d
Asmelash <asmelashtk@gmail.com>
2017-09-27 16:02:57 +0200

    added filter stream.

--- tests/test_twitter.py
+++ tests/test_twitter.py
@@ -4,9 +4,24 @@ import pytest
 
 from rumorly import twitter
 
-stream  = twitter.STREAMING_API(key=1, payload={})
 
 def test_sample_stream():
+    stream  = twitter.STREAMING_API(key=1, payload={})
+    tweets = []
+    for idx, tweet in enumerate(stream.run()):
+        print(tweet)
+        tweets.append(tweet)
+        if idx == 10: break
+
+    assert len(tweets) > idx
+    # inspect tweets
+    for tweet in tweets:
+        print(json.dumps(tweet))
+
+
+
+def test_filter_stream():
+    stream  = twitter.STREAMING_API(key=1, payload={'hashtags':'rt'}, end_point='sample')
     tweets = []
     for idx, tweet in enumerate(stream.run()):
         print(tweet)
